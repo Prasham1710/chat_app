dc614cbd0415fcc19c20abeb9f44e53507b1f604
Prasham1710 <jainprasham17@gmail.com>
2023-03-22 16:07:19 +0530

    create database

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,27 @@
-import React,{useEffect, useRef} from 'react';
+import { query, collection,orderBy,onSnapshot } from 'firebase/firestore';
+import React,{useEffect, useRef, useState} from 'react';
 import Message from './Message';
+import {db} from '../firebase'
 const Chat = () => {
-    const scroll = useRef()
+    const[messages, setMessages] = useState([]);
+    const scroll = useRef();
+
+    useEffect(() => {
+        const  q = query(collection(db, 'messages'), orderBy('timestamp'));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let messages = [];
+      querySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe();
+    },[]);
   return (
   <>
   <main className='flex flex-col p-[10px]'>
+
+    
   <Message/>
   </main>
   {/*Send*/}

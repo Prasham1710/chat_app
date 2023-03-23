0629fb39508fbe4e3326a38405341a397c331d29
Prasham1710 <jainprasham17@gmail.com>
2023-03-23 13:54:22 +0530

    update

--- src/firebase.js
+++ src/firebase.js
@@ -8,8 +8,8 @@ import { getAuth } from "firebase/auth";
 import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBkn1WJsVk4e0aibmrnexSIlIUYAkq753k",
-  authDomain: "chat-app-c0ea3.firebaseapp.com",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,

455bc826e6bcfebe3411adefb86590e1deb4bee3
Prasham1710 <jainprasham17@gmail.com>
2023-03-23 13:50:10 +0530

    env

--- src/firebase.js
+++ src/firebase.js
@@ -4,18 +4,19 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
+
 import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBkn1WJsVk4e0aibmrnexSIlIUYAkq753k",
   authDomain: "chat-app-c0ea3.firebaseapp.com",
-  projectId: "chat-app-c0ea3",
-  storageBucket: "chat-app-c0ea3.appspot.com",
-  messagingSenderId: "210757128437",
-  appId: "1:210757128437:web:c25d2d28f07360c60c75bc"
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
- export const db = getFirestore(app)
+export const db = getFirestore(app)

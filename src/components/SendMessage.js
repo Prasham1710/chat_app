2c9b736c02cd55faeb07045e7c2902cd834b00eb
Prasham1710 <jainprasham17@gmail.com>
2023-03-25 01:11:33 +0530

    firebase

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,40 +1,45 @@
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
-import React, { useState } from 'react'
-import {auth ,db } from '../firebase'
+import React, { useState } from 'react';
+import {auth, db} from '../firebase'
+import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
 
-const SendMessage = (scroll) => {
-const [input, setInput] = useState('');
+const SendMessage = ({scroll}) => {
+  const [input, setInput] = useState('');
 
   const sendMessage = async (e) => {
     e.preventDefault()
-    if (input === '' ) {
-        alert('Please enter a message')
+    if (input === '') {
+        alert('Please enter a valid message')
         return
     }
     const {uid, displayName} = auth.currentUser
-    await addDoc(collection(db, 'messages'),{
+    await addDoc(collection(db, 'messages'), {
         text: input,
-        name : displayName,
+        name: displayName,
         uid,
         timestamp: serverTimestamp()
     })
     setInput('')
-    scroll.current.scrollIntoView({behavior :'smooth'})
-}
+    scroll.current.scrollIntoView({behavior: 'smooth'})
+  }
 
   return (
-    <form  onSubmit={sendMessage} className= 'h-14 w-full max-w-[728px]  flex text-xl absolute bottom-0'>
+    <form onSubmit={sendMessage} className= 'h-14 w-full max-w-[728px] mb-0 flex text-xl absolute bottom-0'>
 
-        <input  
-        vlaue = {input} 
-        onChange={(e) => setInput(e.target.value)} className='w-full text-xl p-3 bg-gray-900 text-white outline-none border-none' 
-        type= "text" placeholder='Message' 
+        <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className='w-full text-xl p-3 bg-gray-900 text-white outline-none border-none' 
+        type='text'
+        placeholder='Message' 
         />
-        <button className='w-[20%] bg-green-500' type='submit'>Send</button>
+        <button className='w-[20%] bg-green-500' 
+        type='submit'>
+            Send
+        </button>
 
     </form>
 
   )
 }
 
-export default SendMessage
+export default SendMessage;
